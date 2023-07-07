d1380dbd7af609b42aff7c6e014d4a632138f54a
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-07-07 00:57:56 -0500

    Update wh_report_per.py

--- wh_report_per.py
+++ wh_report_per.py
@@ -257,8 +257,8 @@ TODAY = datetime.datetime.now(timezone(client_timezone)).strftime("%Y-%m-%d") \
     if option == "Today" \
     else datetime.datetime.now(timezone(client_timezone)) - datetime.timedelta(days=1)
 returns_df["status_time"] = returns_df["status_time"].apply(lambda a: a.strftime("%Y-%m-%d %H:%M:%S"))
-#returns_df["point_C_time"] = returns_df["point_C_time"].apply(lambda a: a if a == "Point C was never visited" else a.strftime("%Y-%m-%d %H:%M:%S"))
-returns_df["point_C_time"] = returns_df["point_C_time"].apply(lambda a: a.strftime("%Y-%m-%d %H:%M:%S"))
+returns_df["point_C_time"] = returns_df["point_C_time"].apply(lambda a: a if a == "Point C was never visited" else a.strftime("%Y-%m-%d %H:%M:%S"))
+#returns_df["point_C_time"] = returns_df["point_C_time"].apply(lambda a: a.strftime("%Y-%m-%d %H:%M:%S"))
 returns_df["created_time"] = returns_df["created_time"].apply(lambda a: pandas.to_datetime(a).date()).reindex()
 with pandas.ExcelWriter(FILE_BUFFER, engine='xlsxwriter') as writer:
     
