9d53b22e7e3d568c9e480e833813b91ffa5c088c
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-07-06 22:34:26 -0500

    Update wh_report_per.py

--- wh_report_per.py
+++ wh_report_per.py
@@ -181,7 +181,7 @@ def get_report(option="Today", start_=None, end_=None) -> pandas.DataFrame:
         timezone(client_timezone))
                 report_point_C_time = report_point_C_time.strftime("%Y-%m-%d %H:%M:%S")
             except Exception as error:
-                report_point_C_time = "Point C was never visited"+e    
+                report_point_C_time = "Point C was never visited"+error    
             row = [report_cutoff, report_created_time, report_client, report_client_id, report_barcode, report_claim_id, report_lo_code, report_status, report_status_time, 
                    report_pod_point_id, report_pickup_address, report_receiver_address, report_receiver_phone, report_receiver_name, report_comment,
                    report_courier_name, report_courier_park,
